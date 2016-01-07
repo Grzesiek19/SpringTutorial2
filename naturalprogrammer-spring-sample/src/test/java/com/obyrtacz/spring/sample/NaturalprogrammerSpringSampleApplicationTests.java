9c3f8ed87a5cd3479176305bb7d65a485107945a
Grzesiek19 <grzesyou@wp.pl>
2016-01-07 16:50:45 +0100

    Initial commit

---
+++ naturalprogrammer-spring-sample/src/test/java/com/obyrtacz/spring/sample/NaturalprogrammerSpringSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.obyrtacz.spring.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NaturalprogrammerSpringSampleApplication.class)
+public class NaturalprogrammerSpringSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
