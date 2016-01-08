f9a6fed80055be0cd958018ad2729fca88c8e75b
Grzesiek19 <grzesyou@wp.pl>
2016-01-08 23:32:35 +0100

    After 6th lecture.

--- naturalprogrammer-spring-sample/src/main/java/com/obyrtacz/spring/sample/core/MailController.java
+++ naturalprogrammer-spring-sample/src/main/java/com/obyrtacz/spring/sample/core/MailController.java
@@ -1,5 +1,7 @@
 package com.obyrtacz.spring.sample.core;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,8 @@ import com.obyrtacz.spring.sample.mail.MockMailSender;
 @RestController
 public class MailController {
 
-	
-	private MailSender mailSender = new MockMailSender();
+	@Resource
+	private MailSender mailSender;
 	
 	@RequestMapping("/mail")
 	public String sendMail(){
