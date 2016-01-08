b2292780a74260b43f54fb38a2e85c25b476d515
Grzesiek19 <grzesyou@wp.pl>
2016-01-09 00:24:27 +0100

    After 7th lecture.

--- naturalprogrammer-spring-sample/src/main/java/com/obyrtacz/spring/sample/mail/MockMailSender.java
+++ naturalprogrammer-spring-sample/src/main/java/com/obyrtacz/spring/sample/mail/MockMailSender.java
@@ -4,7 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("mailSender")
 public class MockMailSender implements MailSender {
 	
 	private static final Log log = LogFactory.getLog(MockMailSender.class);
@@ -14,7 +14,7 @@ public class MockMailSender implements MailSender {
 	 */
 	@Override
 	public void send(String to, String subject, String body){
-		log.info("Sending email to: " + to);
+		log.info("Sending  email to: " + to);
 		log.info("Subject: "+ subject);
 		log.info("Body: "+body);
 	}
